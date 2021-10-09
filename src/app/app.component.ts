e68baaea619571eeb762ff64d4de78717e53d1af
wdmuthiora <wdmuthiora@gmail.com>
2021-10-09 14:30:00 +0300

    Angular deployment exercise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+//Import component from Angular
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
+  //selector is a custom html element
   selector: 'app-root',
+  //template and css build up the view to be called with the custom selector above
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+  
+
 export class AppComponent {
-  title = 'Goals';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+//Import modules from Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+//Import local modules and components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
+
+
+//DECLARATIONS OF modules, services and components
 @NgModule({
+  //declare components
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+
+  //declare modules
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+
+  //declare services
   providers: [],
-  bootstrap: [AppComponent]
+
+  //declare ???
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
